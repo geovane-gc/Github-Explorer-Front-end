08924f7e17169c74138d021b12a0746c67d8229e
Geovane <geovanegallotticazal@hotmail.com>
2020-10-14 15:04:25 -0300

    started api connection

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github.</Title>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
 
-    <Form action="">
-      <input placeholder="Digite o nome do repositório" type="text" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+  function handleAddRepository(): void {
+    console.log(newRepo);
+  }
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars2.githubusercontent.com/u/58887847?s=460&u=9934afaad73f2863facf9ba1ff9c2f747e6d276d&v=4"
-          alt="Geovane Gallotti"
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
         />
-        <div>
-          <strong>rocketseat/unform/</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/58887847?s=460&u=9934afaad73f2863facf9ba1ff9c2f747e6d276d&v=4"
+            alt="Geovane Gallotti"
+          />
+          <div>
+            <strong>rocketseat/unform/</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
